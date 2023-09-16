0b2cbd589c32e1798804066ed7d2631cbf8bb7f6
Yash Baravaliya <110822197+YashBaravaliya@users.noreply.github.com>
2023-09-16 12:50:30 +0530

    upload project

---
+++ app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+from PIL import Image, ImageDraw, ImageFont
+import os
+
+# Function to generate the sign language GIF
+def generate_sign_language_gif(input_text):
+    # Folder containing sign language images (A to Z)
+    image_folder = "sign_language_images"
+
+    # Split the input text into words
+    words = input_text.split()
+
+    # Create a list of image filenames for each letter in the input text
+    image_filenames = []
+
+    for letter in input_text:
+        if " " in letter:
+            letter = "space"
+        image_filenames.append(os.path.join(image_folder, letter + ".jpeg"))
+
+    # Create a GIF by combining the images
+    images = [Image.open(filename) for filename in image_filenames]
+    images[0].save("sign_language.gif", save_all=True, append_images=images[1:], loop=0, duration=500)
+
+    return "sign_language.gif"
+
+# Main Streamlit app code
+def main():
+    st.title("Fantastic Sign Language Generator 🤩")
+
+    # Input text from the user
+    input_text = st.text_input("Enter text:")
+    
+    if input_text:
+        # Generate the sign language GIF
+        gif_path = generate_sign_language_gif(input_text)
+        
+        # Display the generated GIF
+        st.image(gif_path, use_column_width=True, caption="Sign Language GIF")
+        
+        # Add some fantastic text with emojis
+        st.markdown("This is a fantastic sign language GIF! ✌️👏🎉")
+
+if __name__ == "__main__":
+    main()
